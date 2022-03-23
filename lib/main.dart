1d2aadbaafdfa3fc95dbfeaf6d87f8a9df353d81
Greg Ng <gregng14@gmail.com>
2022-03-23 11:23:28 -0600

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flash_chat_flutter/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
